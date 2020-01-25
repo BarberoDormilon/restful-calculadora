862d12a5d78ae14092f35ef9a1c05b5602b90d27
BarberoDormilon <larrycardenaszavaleta@gmail.com>
2020-01-26 00:06:29 +0100

    Proyecto base calculadora

---
+++ src/test/java/com/restful/calculadora/restfulcalculadora/RestfulCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.calculadora.restfulcalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
