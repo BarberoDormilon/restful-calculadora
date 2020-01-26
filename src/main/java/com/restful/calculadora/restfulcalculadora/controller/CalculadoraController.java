f694a6d68c673dae304be35730530da7ef4327c5
BarberoDormilon <larrycardenaszavaleta@gmail.com>
2020-01-26 20:45:23 +0100

    Peticiones get suma y resta

--- src/main/java/com/restful/calculadora/restfulcalculadora/controller/CalculadoraController.java
+++ src/main/java/com/restful/calculadora/restfulcalculadora/controller/CalculadoraController.java
@@ -9,9 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/calculadora")
 public class CalculadoraController {
 	
-	@GetMapping(path="/{num1}/{num2}")
+	@GetMapping(path="/suma/{num1}/{num2}")
 	public String Sumar(@PathVariable String num1, @PathVariable String num2) {
 		return "La suma es: "+(Integer.parseInt(num1)+Integer.parseInt(num2));
+	}
+	
+	@GetMapping(path="/resta/{num1}/{num2}")
+	public String Resta(@PathVariable String num1, @PathVariable String num2) {
+		return "La resta es: "+(Integer.parseInt(num1)-Integer.parseInt(num2));
 	}
 
 }
