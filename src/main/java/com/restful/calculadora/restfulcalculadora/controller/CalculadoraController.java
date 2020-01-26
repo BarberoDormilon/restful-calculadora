280740927cdffd44d59c577cbe7c63928b501ba8
BarberoDormilon <larrycardenaszavaleta@gmail.com>
2020-01-26 20:05:41 +0100

    leer variable en peticion get

--- src/main/java/com/restful/calculadora/restfulcalculadora/controller/CalculadoraController.java
+++ src/main/java/com/restful/calculadora/restfulcalculadora/controller/CalculadoraController.java
@@ -1,15 +1,17 @@
 package com.restful.calculadora.restfulcalculadora.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculadora")
 public class CalculadoraController {
-	@GetMapping
-	public String Sumar() {
-		return "Opcion Sumar";
+	
+	@GetMapping(path="/{num1}")
+	public String Sumar(@PathVariable String num1) {
+		return "Variable recibida: "+num1;
 	}
 
 }
